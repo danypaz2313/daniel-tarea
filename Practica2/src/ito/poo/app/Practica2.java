f5600cef247f43d69135b62dfde5de6aaba4fcc2
Jose Maria CL <chema9703@gmail.com>
2021-09-13 19:56:05 -0500

    Copy teacher repo

---
+++ Practica2/src/ito/poo/app/Practica2.java
@@ -0,0 +1,69 @@
+package ito.poo.app;
+
+import java.util.Scanner;
+
+
+public class Practica2 {
+
+	static Scanner input= new Scanner(System.in);
+	//=============================================
+	// Este método debe leer desde el teclado
+	// cada uno de los elementos de un arreglo
+	// que es pasado como argumento.
+	//=============================================
+	static void leerArreglo (int [] a) {
+		
+	}
+	
+	//=============================================
+	// Este método debe retornar el número de tareas
+	// que han vencido en función del día (hoy) pasado
+	// como argumento.
+	// =============================================
+	static int cantidaTareasVencidas(int [] a , int hoy) {
+		int i=0;
+		return i;
+	}
+	// =============================================
+	// Este método debe retornar la lista de tareas que 
+	// han vencido al día (hoy).
+	// =============================================
+	static int [] tareasVencidas(int [] a, int hoy) {
+		int [] lv= {};
+		return lv;
+	}
+	
+	static int cantidadTareasProximas(int [] a, int hoy) {
+		int i=0;
+		return i;
+	}
+	
+	static int [] tareasProximas(int [] a, int hoy) {
+		int [] lv= {};
+		return lv;
+	}
+	
+	static int cantidadTareasAReprogramar(int [] a, int hoy) {
+		int i=0;
+		return i;
+	}
+	
+	static int [] tareasAReprogramar(int [] a, int hoy) {
+		int [] lv= {};
+		return lv;
+	}
+	
+	static void imprimeResultados(int [] a) {
+		
+	}
+	
+	static void run() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        run();
+	}
+
+}
